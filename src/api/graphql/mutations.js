cadcb840e4a92e74d149a6c9aa3854d5b2d9b830
Tim Tan <timmytan18@gmail.com>
2021-12-26 18:15:32 -0600

    Add GraphQL mutations and subscriptions

---
+++ src/api/graphql/mutations.js
@@ -0,0 +1,40 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createFeastItem = /* GraphQL */ `
+  mutation CreateFeastItem(
+    $input: CreateFeastItemInput!
+    $condition: ModelFeastItemConditionInput
+  ) {
+    createFeastItem(input: $input, condition: $condition) {
+      PK
+    }
+  }
+`;
+export const updateFeastItem = /* GraphQL */ `
+  mutation UpdateFeastItem(
+    $input: UpdateFeastItemInput!
+    $condition: ModelFeastItemConditionInput
+  ) {
+    updateFeastItem(input: $input, condition: $condition) {
+      PK
+    }
+  }
+`;
+export const incrementFeastItem = /* GraphQL */ `
+  mutation IncrementFeastItem($input: IncrementFeastItemInput!) {
+    incrementFeastItem(input: $input) {
+      PK
+    }
+  }
+`;
+export const deleteFeastItem = /* GraphQL */ `
+  mutation DeleteFeastItem(
+    $input: DeleteFeastItemInput!
+    $condition: ModelFeastItemConditionInput
+  ) {
+    deleteFeastItem(input: $input, condition: $condition) {
+      PK
+    }
+  }
+`;
