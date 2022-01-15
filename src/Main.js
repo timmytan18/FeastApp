c28a15cc3f6e66dc13b6364483a446bf812413e4
Tim Tan <timmytan18@gmail.com>
2022-01-15 01:14:26 -0500

    Main.js eslint

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useContext } from 'react';
 import { View, Image } from 'react-native';
 
-import Amplify, { Auth, Hub, API, graphqlOperation } from 'aws-amplify';
+import Amplify, {
+  Auth, Hub, API, graphqlOperation,
+} from 'aws-amplify';
 import awsconfig from './aws-exports';
 
 import { getUserProfile } from './api/graphql/queries';
@@ -14,100 +16,102 @@ import { Context } from './Store';
 Amplify.configure(awsconfig);
 
 const Main = () => {
+  const [state, dispatch] = useContext(Context);
 
-    const [state, dispatch] = useContext(Context);
+  useEffect(() => {
+    // listen to changes in sign in status
+    Hub.listen('auth', ({ payload: { event, data } }) => {
+      switch (event) {
+        case 'signIn':
+          console.log('sign in');
+          getUser().catch((e) => noUser(e));
+          break;
+        case 'signOut':
+          console.log('sign out');
+          noUser();
+          break;
+      }
+    });
 
-    useEffect(() => {
-        // listen to changes in sign in status
-        Hub.listen("auth", ({ payload: { event, data } }) => {
-            switch (event) {
-                case "signIn":
-                    console.log('sign in')
-                    getUser().catch((e) => noUser(e))
-                    break;
-                case "signOut":
-                    console.log('sign out')
-                    noUser()
-                    break;
-            }
-        });
-
-        getUser().catch((e) => noUser(e))
-
-        async function updateIdentityId(PK, SK) {
-            const currentCreds = await Auth.currentCredentials();
-            console.log(currentCreds)
-            const identityId = currentCreds.identityId;
-            try {
-                await API.graphql(graphqlOperation(
-                    updateFeastItem,
-                    { input: { PK: PK, SK: SK, identityId: identityId } }
-                ));
-            } catch(err) {
-                console.log(err)
-            }
-            return identityId;
-        }
-
-        async function getUser() {
-
-            // Fetch user Cognito credentials
-            const cognitoUser = await Auth.currentAuthenticatedUser();
-            console.log(cognitoUser)
-            const id = cognitoUser.attributes.sub;
-            const pk = `USER#${id}`;
-            const sk = '#PROFILE#';
-
-            // Fetch user profile from DynamoDB
-            let dynamoUser;
-            try {
-                dynamoUser = await API.graphql(graphqlOperation(
-                  getUserProfile, { PK: pk, SK: { beginsWith: sk } }
-                ));
-            } catch (err) {
-                console.log(err)
-            }
-
-            // identityId (S3), city, picture can be null
-            const { PK, SK, uid, name, identityId, city, picture } = dynamoUser.data.listFeastItems.items[0];
-            if (!identityId) {
-                identityId = await updateIdentityId(PK, SK)
-            }
-            const user = { PK, SK, uid, name, identityId, city, picture };
-            console.log('User profile:', user)
-
-            if (picture) {
-                try {
-                    await Image.prefetch(picture)
-                } catch (e) {
-                    console.log("Error prefetching profile picture:", e)
-                }
-            }
-
-            // Put user in global state
-            dispatch({ type: 'SET_USER', payload: user })
-        }
-
-        function noUser(e) {
-            console.log("Not signed in")
-            console.log(e)
-            dispatch({ type: 'SET_USER', payload: 'none' })
-        }
-    }, [])
-    
-    if (!state.user) {
-        return (null);
+    async function updateIdentityId(PK, SK) {
+      const currentCreds = await Auth.currentCredentials();
+      console.log(currentCreds);
+      const { identityId } = currentCreds;
+      try {
+        await API.graphql(graphqlOperation(
+          updateFeastItem,
+          { input: { PK, SK, identityId } },
+        ));
+      } catch (err) {
+        console.log(err);
+      }
+      return identityId;
     }
 
-    return (
-        <View style={{ flex: 1, backgroundColor: '#F9F9F9' }}>
-            {state.user === 'none' ? (
-                <AuthNavigator />
-            ) : (
-                <AppNavigator />
-            )}
-        </View>
-    );
-}
+    async function getUser() {
+      // Fetch user Cognito credentials
+      const cognitoUser = await Auth.currentAuthenticatedUser();
+      console.log(cognitoUser);
+      const id = cognitoUser.attributes.sub;
+      const pk = `USER#${id}`;
+      const sk = '#PROFILE#';
+
+      // Fetch user profile from DynamoDB
+      let dynamoUser;
+      try {
+        dynamoUser = await API.graphql(
+          graphqlOperation(getUserProfile, { PK: pk, SK: { beginsWith: sk } }),
+        );
+      } catch (err) {
+        console.log(err);
+      }
+
+      // identityId (S3), city, picture can be null
+      const {
+        PK, SK, uid, name, identityId, city, picture,
+      } = dynamoUser.data.listFeastItems.items[0];
+      if (!identityId) {
+        identityId = await updateIdentityId(PK, SK);
+      }
+      const user = {
+        PK, SK, uid, name, identityId, city, picture,
+      };
+      console.log('User profile:', user);
+
+      if (picture) {
+        try {
+          await Image.prefetch(picture);
+        } catch (e) {
+          console.log('Error prefetching profile picture:', e);
+        }
+      }
+
+      // Put user in global state
+      dispatch({ type: 'SET_USER', payload: user });
+    }
+
+    function noUser(e) {
+      console.log('Not signed in');
+      console.log(e);
+      dispatch({ type: 'SET_USER', payload: 'none' });
+    }
+
+    getUser().catch((e) => noUser(e));
+  }, []);
+
+  if (!state.user) {
+    return (null);
+  }
+
+  return (
+    <View style={{ flex: 1, backgroundColor: '#F9F9F9' }}>
+      {state.user === 'none' ? (
+        <AuthNavigator />
+      ) : (
+        <AppNavigator />
+      )}
+    </View>
+  );
+};
 
 export default Main;
