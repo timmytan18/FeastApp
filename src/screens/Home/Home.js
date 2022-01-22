c97001e2d4e4ac2e061f2a5a0af8caf7d8ed65bd
Tim Tan <timmytan18@gmail.com>
2022-01-22 18:05:42 -0500

    Display user reviews on Home screen
    - Fetch following list and places reviewed for all following
    - Display info on map

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,30 +1,44 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
 import * as Location from 'expo-location';
 import MapView from 'react-native-maps';
-// import { useHeaderHeight } from '@react-navigation/stack';
-// import { useScrollToTop } from '@react-navigation/native';
-// import { useCollapsibleHeader } from 'react-navigation-collapsible';
-// import ReadMore from '@fawazahmed/react-native-read-more';
-// import { LinearGradient } from 'expo-linear-gradient';
-// import Stars from 'react-native-stars';
-// import { StarFull, StarHalf, StarEmpty } from '../components/util/icons/Star';
+import geohash from 'ngeohash';
+import { getFollowingQuery, getUserReviewsQuery } from '../../api/functions/queryFunctions';
 import SearchButton from '../components/util/SearchButton';
-// import Ratings from '../components/Ratings';
-// import MyCarousel from '../Carousel';
-// import Lightbox from '../components/Lightbox';
-// import MapMarker from '../components/util/icons/MapMarker';
-// import YumButton from '../components/util/YumButton';
-// import Line from '../components/util/Line';
 import { Context } from '../../Store';
 import { colors, hp, wp } from '../../constants/theme';
 
 const Home = ({ navigation }) => {
   const [state, dispatch] = useContext(Context);
+  const [region, setRegion] = useState(null);
+  const initialDelta = {
+    latitudeDelta: 0.0461, // ~3.5 miles in height
+    longitudeDelta: 0.02105,
+  };
+  const geoRange = {
+    dLat: 0.3688, // ~28 miles in height
+    dLng: 0.1684,
+  };
+  // const geoRange = {
+  //   dLat: 0.1844, // ~14 miles in height
+  //   dLng: 0.0842,
+  // };
+  // const geoRange = {
+  //   dLat: 0.0922, // ~7 miles in height
+  //   dLng: 0.0421,
+  // };
+  // const geoRange = {
+  //   dLat: 0.0461, // ~3.5 miles in height
+  //   dLng: 0.02105,
+  // };
+
+  const [reviews, setReviews] = useState(null);
+  const [markers, setMarkers] = useState([]);
 
   useEffect(() => {
     (async () => {
+      // Get user location
       const { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
         console.log('Permission to access location was denied');
@@ -36,12 +50,53 @@ const Home = ({ navigation }) => {
       dispatch({
         type: 'SET_LOCATION',
         payload: {
-          longitude: coords.longitude,
           latitude: coords.latitude,
+          longitude: coords.longitude,
         },
       });
+
+      // Get user's following users
+      const followingUsers = await getFollowingQuery({ uid: state.user.uid });
+      console.log(followingUsers);
+
+      // Get geohashes from user location
+      const { dLat, dLng } = geoRange;
+      const bottomLeft = { lat: coords.latitude - dLat, lng: coords.longitude - dLng };
+      const topRight = { lat: coords.latitude + dLat, lng: coords.longitude + dLng };
+      const precision = 3; // ~3 mile grid
+      const hashes = geohash.bboxes(bottomLeft.lat, bottomLeft.lng, topRight.lat, topRight.lng, precision);
+      console.log(hashes.join(' '));
+
+      // Get following users' reviews
+      const followingReviews = {};
+      const placeMarkers = [{ name: 'Me', lat: coords.latitude, lng: coords.longitude }];
+      await Promise.all(followingUsers.map(async (user) => {
+        await Promise.all(hashes.map(async (hash) => {
+          const currReviews = await getUserReviewsQuery({ PK: user.PK, hash, withUserInfo: true });
+          for (let i = 0; i < currReviews.length; i += 1) {
+            currReviews[i].coordinates = geohash.decode(currReviews[i].geo);
+            const {
+              placeId,
+              coordinates: { latitude: lat, longitude: lng },
+              name,
+              placeUserInfo: { picture: userPic },
+            } = currReviews[i];
+            if (placeId in followingReviews) {
+              followingReviews[placeId].push(currReviews[i]);
+            } else {
+              followingReviews[placeId] = [currReviews[i]];
+              placeMarkers.push({
+                name, lat, lng, userPic,
+              });
+            }
+          }
+        }));
+      }));
+      setReviews(followingReviews);
+      console.log(followingReviews);
+      setMarkers(placeMarkers);
     })();
-  }, [dispatch]);
+  }, [dispatch, state.user.uid]);
 
   if (!state.location) {
     return (null);
@@ -50,23 +105,34 @@ const Home = ({ navigation }) => {
   SplashScreen.hideAsync();
 
   const { latitude, longitude } = state.location;
-
-  const region = {
+  const initialRegion = {
     latitude,
     longitude,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
+    ...initialDelta,
   };
+  if (!region) {
+    setRegion(initialRegion);
+  }
 
-  const markers = [{ latlng: { latitude, longitude } }];
+  const onRegionChange = (newRegion) => {
+    console.log(newRegion);
+    setRegion(newRegion);
+  };
 
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} region={region}>
+      <MapView
+        style={styles.map}
+        region={region}
+        // onRegionChangeComplete={onRegionChange}
+        rotateEnabled={false}
+      >
         {markers.map((marker) => (
           <MapView.Marker
-            key={`${marker.latlng.latitude}${marker.latlng.longitude}`}
-            coordinate={marker.latlng}
+            key={`${marker.lat}${marker.lng}`}
+            coordinate={{ latitude: marker.lat, longitude: marker.lng }}
+            title={marker.name}
+            image={{ uri: marker.userPic }}
           />
         ))}
       </MapView>
