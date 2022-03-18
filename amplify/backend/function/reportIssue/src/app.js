80fc70e29f58e60ed29d065f3ac177968c562abe
Tim Tan <timmytan18@gmail.com>
2022-03-18 15:40:07 -0400

    Amplify push

--- amplify/backend/function/reportIssue/src/app.js
+++ amplify/backend/function/reportIssue/src/app.js
@@ -1,17 +1,3 @@
-/*
-Use the following code to retrieve configured secrets from SSM:
-
-const aws = require('aws-sdk');
-
-const { Parameters } = await (new aws.SSM())
-  .getParameters({
-    Names: ["twiliokey"].map(secretName => process.env[secretName]),
-    WithDecryption: true,
-  })
-  .promise();
-
-Parameters will be of the form { Name: 'secretName', Value: 'secretValue', ... }[]
-*/
 const aws = require('aws-sdk');
 
 const REGION = 'us-east-2';
